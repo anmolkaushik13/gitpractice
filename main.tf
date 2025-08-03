03155d44628bbb7b713c7886ccb4c8ed853ff44c
Anmol Kaushik <anmolkaushik13042000@gmail.com>
2025-08-03 23:54:27 +0530

    aa

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "ak_rg" {
   name = "ak_rg"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "ak_rg" {
+  name = "ak_rg"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "ak_rg" {
+  name = "ak_rg"
+  location = "eastus"
+}
